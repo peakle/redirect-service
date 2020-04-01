51262096bcc10890bc794090b3ea4e0017ae0195
Denis Limarev <dlimarev@wakeapp.ru>
2020-04-01 23:49:34 +0300

    add logic

--- pkg/provider/provider.go
+++ pkg/provider/provider.go
@@ -1,7 +1,77 @@
 package provider
 
-import "github.com/urfave/cli"
+import (
+	"errors"
+	"fmt"
+	"github.com/valyala/fasthttp"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+	"time"
+)
 
-func Create(c *cli.Context) {
+const ApiMessage = "https://api.vk.com/method/messages.send?"
+const ApiVersion = "5.101"
 
+func Create(uri string) (string, error) {
+	var err error
+	_, err = url.ParseRequestURI(uri)
+	if err != nil {
+		return "", err
+	}
+
+	t := make([]byte, 7)
+	var r string
+
+	for i := 0; ; i++ {
+		rand.Read(t)
+		r = fmt.Sprintf("%x", t)
+		if !tokenExist(r) {
+			break
+		}
+
+		time.Sleep(time.Second)
+		if i == 10 {
+			return "", errors.New("ошибка создания")
+		}
+	}
+
+	return r, nil
+}
+
+func FindUrl(url string) (string, error) {
+	if url == "" {
+		return "", nil
+	}
+
+	return "", nil
+}
+
+func RecordStats(ctx *fasthttp.RequestCtx) {
+	time.Sleep(time.Second * 100)
+	fmt.Println(123)
+}
+
+func SendMessageVK(userId, message string) {
+	rand.Seed(time.Now().Unix())
+	randomId := strconv.Itoa(rand.Intn(10000))
+
+	urlArgs := url.Values{}
+	urlArgs.Add("user_id", userId)
+	urlArgs.Add("random_id", randomId)
+	urlArgs.Add("access_token", os.Getenv("VK_TOKEN"))
+	urlArgs.Add("message", message)
+	urlArgs.Add("v", ApiVersion)
+	urlInfo := urlArgs.Encode()
+
+	_, err := http.Get(ApiMessage + urlInfo)
+	if err != nil {
+		fmt.Printf("error in send message: %v", err)
+	}
+}
+
+func tokenExist(token string) bool {
+	return false
 }
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,17 +1,45 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/urfave/cli"
 	"github.com/valyala/fasthttp"
-	"net/url"
+	"gitlab.com/Peakle/redirect-service/pkg/provider"
+	"os"
+	"strconv"
 )
 
+const (
+	VkGroupId    = 177387678
+	Confirmation = "confirmation"
+	MessageNew   = "message_new"
+)
+
+var secret string
+var vkConfirmationToken string
+
+type vkMessage struct {
+	Secret  string          `json:"secret"`
+	GroupId int             `json:"group_id"`
+	Type    string          `json:"type"`
+	Object  vkMessageObject `json:"object"`
+}
+
+type vkMessageObject struct {
+	FromId int    `json:"from_id"`
+	Text   string `json:"text"`
+}
+
 func StartServer(c *cli.Context) {
+	fmt.Println("Start server...")
+	secret = os.Getenv("VK_API_SECRET")
+	vkConfirmationToken = os.Getenv("VK_CONFIRMATION_TOKEN")
+
 	requestHandler := func(ctx *fasthttp.RequestCtx) {
 		switch string(ctx.Path()) {
-		case "/create":
-			handleCreate(ctx)
+		case "/vk":
+			handleVK(ctx)
 		default:
 			handleRedirect(ctx)
 		}
@@ -24,13 +52,57 @@ func StartServer(c *cli.Context) {
 }
 
 func handleRedirect(ctx *fasthttp.RequestCtx) {
-	//var err error
+	var err error
+	ctx.Response.Header.Set("Content-Type", "text/html; charset=utf-8")
 
-	uri, _ := url.ParseRequestURI(string(ctx.Request.RequestURI()))
+	redirectUri, err := provider.FindUrl(string(ctx.Request.RequestURI()))
+	if err != nil {
+		_, _ = fmt.Fprint(ctx, "")
+	}
 
-	fmt.Print(uri.RawQuery)
+	ctx.Response.Header.Set("Location", redirectUri)
+	ctx.Response.SetStatusCode(301)
+	ctx.SetConnectionClose()
+
+	provider.RecordStats(ctx)
 }
 
-func handleCreate(ctx *fasthttp.RequestCtx) {
+func handleVK(ctx *fasthttp.RequestCtx) {
+	var err error
 
+	message := vkMessage{}
+	err = json.Unmarshal(ctx.Request.Body(), &message)
+	if err != nil {
+		fmt.Printf("error occurred on parse vk message: %v", err)
+		_, _ = fmt.Fprint(ctx, "")
+		return
+	}
+
+	if message.Secret != secret || len(message.Secret) == 0 {
+		_, _ = fmt.Fprint(ctx, "")
+		return
+	}
+
+	switch message.Type {
+	case Confirmation:
+		if message.GroupId != VkGroupId {
+			_, _ = fmt.Fprint(ctx, "")
+			return
+		}
+
+		_, _ = fmt.Fprint(ctx, vkConfirmationToken)
+	case MessageNew:
+		_, _ = fmt.Fprint(ctx, "ok")
+
+		var r string
+		r, err = provider.Create(message.Object.Text)
+		if err != nil {
+			r = "невалидный url"
+		}
+
+		provider.SendMessageVK(strconv.Itoa(message.Object.FromId), r)
+	default:
+		_, _ = fmt.Fprint(ctx, "")
+	}
+	ctx.SetConnectionClose()
 }
